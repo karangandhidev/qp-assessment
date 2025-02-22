62c9a6048c56261d4b5e1173a38eb86afe0a26a9
Karan Gandhi <gandhik199@gmail.com>
2025-02-22 14:58:06 +0530

    Importing everything together

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,3 +1,11 @@
 import * as users from "./Users";
+import * as grocery from "./Grocery";
+import * as orders from "./Orders";
+import * as orderItems from "./OrderItems";
 
-export default { users };
+export default {
+  users: users,
+  grocery: grocery,
+  orders: orders,
+  orderItems: orderItems,
+};
